474cf0ff8881901a56eb94f2000f8fac0b16743d
Rodrigogmdev <rodrigogarciamoraesdev@gmail.com>
2025-10-19 02:49:04 -0300

    migração do DB

--- backend/database.py
+++ backend/database.py
@@ -1,17 +1,27 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# 1. caminho do banco de dados SQLite
-SQLALCHEMY_DATABASE_URL = "sqlite:///./quiz.db"
 
-# 2. conexão com SQLAlchemy
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+load_dotenv()
 
-# 3. fábrica de sessões para interagir com o banco
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+if SQLALCHEMY_DATABASE_URL is None:
+  
+    print("AVISO: DATABASE_URL não definida. A usar o banco de dados SQLite local.")
+    SQLALCHEMY_DATABASE_URL = "sqlite:///./quiz.db"
+    
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    )
+else:
+    if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# 4. classe Base que  modelos da tabela irão herdar
 Base = declarative_base()
